9bafdb0cc81635a5e74422e79f8908a9b7fe2f44
unknown <MTBriones@30WY-MTBriones.smart.LOCAL>
2015-09-11 19:46:42 +0800

    remove some uneeded library definition

--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 // Base library import
 var express = require('express'),
-    bodyParser = require('body-parser'),
-    compression = require('compression'),
     app = express();
 
-// Custom class library import
-var offerSvc = require('./server/offers');
-
 app.set('port', process.env.PORT || 3000);
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(compression());
 
 app.use('/', express.static(__dirname + '/www'));
 
